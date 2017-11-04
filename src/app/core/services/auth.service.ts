f9a3f476f1091760f317cd063afb63642f9fe15c
Adrian Eyre <adrian.eyre@hotmail.co.uk>
2017-11-04 18:53:54 +0000

    changed to product to see if that fixes iPhone

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -9,7 +9,8 @@ export class AuthService {
   private BASE_URL: string = 'http://localhost:5000/auth';
   private headers: Headers = new Headers({'Content-Type': 'application/json'});
 
-  constructor(private http: Http) {}
+  constructor(
+    private http: Http) {}
 
   login(user: User): Promise<any> {
     const url: string = `${ this.BASE_URL }/login`;
